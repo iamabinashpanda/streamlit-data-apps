83e6015a78d5d0a2070f3c71dd1b7c5b917c1198
Abinash Panda <iamabinashpanda@outlook.com>
2026-02-16 19:52:47 +0530

    LnD 2

--- LnD/main.py
+++ LnD/main.py
@@ -5,5 +5,11 @@ st.markdown("---")
 
 tab1,tab2 = st.tabs(["Tab 1", "Tab 2"])
 
-if st.button("Submit"):
-    st.balloons()
+tab1.write('Hello World')
+
+st.header('st.button')
+
+if st.button("Click me"):
+    st.write('Click me')
+else:
+    st.write('Hello World')
